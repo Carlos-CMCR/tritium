8081d31376a50101bbfab9899ff772fbc315cabf
Carlos <carloscarbajalrojas@hotmail.com>
2022-10-31 16:29:50 -0500

    auth created

---
+++ resources/js/components/ListUsers.vue
@@ -0,0 +1,22 @@
+<template>
+  <DataTable class="display" :data="data" class="display">
+    <thead>
+      <tr>
+        <th>First</th>
+        <th>Second</th>
+      </tr>
+    </thead>
+  </DataTable>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import DataTable from "datatables.net-vue3";
+// import Select from "datatables.net-select";
+// DataTable.use(Select);
+let data = ref([]);
+data = axios.get('')
+</script>
+
+<style>
+</style>
